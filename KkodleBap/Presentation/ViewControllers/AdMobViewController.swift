b7ba45000f991f17b989fab20c0d9afef21d23e0
gom1n <alice436777@gmail.com>
2025-09-08 21:40:35 +0900

    [fix] ATT 허용 문구 설정 및 info.plist 광고 ID 재설정

--- KkodleBap/Presentation/ViewControllers/AdMobViewController.swift
+++ KkodleBap/Presentation/ViewControllers/AdMobViewController.swift
@@ -49,7 +49,8 @@ class AdMobViewController: UIViewController, FullScreenContentDelegate {
     func loadRewardedAd() async {
         do {
             rewardedAd = try await RewardedAd.load(
-                // 광고 앱 ID
+                // 보상형 광고 앱 ID
+                // 테스트용: ca-app-pub-3940256099942544~1458002511
                 with: "ca-app-pub-8570424711351250~9069249695", request: Request())
             rewardedAd?.fullScreenContentDelegate = self
         } catch {
